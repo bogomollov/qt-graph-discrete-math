cc994ebced29152892e4330c98c06592cf2a80d1
bogomollov <bogdan.bogomolov.2016@mail.ru>
2026-04-29 16:17:59 +0500

    feat: add graph vertex on click

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QQuickWidget>
+#include <QVBoxLayout>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , graphView(new QQuickWidget(this))
 {
     ui->setupUi(this);
+    setWindowTitle(tr("qt-graph-discrete-math"));
+
+    auto *layout = new QVBoxLayout(ui->centralwidget);
+    layout->setContentsMargins(0, 0, 0, 0);
+
+    graphView->setResizeMode(QQuickWidget::SizeRootObjectToView);
+    graphView->setSource(QUrl(QStringLiteral("qrc:/qt/qml/GraphEditor.qml")));
+    layout->addWidget(graphView);
 }
 
 MainWindow::~MainWindow()
